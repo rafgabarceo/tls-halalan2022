e6188e98cac44c23c9a8541dea397466cec9357d
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-03-19 08:18:03 -0700

    Added REST API call function

--- php/rest.php
+++ php/rest.php
@@ -2,10 +2,42 @@
     if(session_status() == PHP_SESSION_NONE){
         session_start();
         $_SESSION["TIME_ACCESSED"] = time();
+        fetchInfo("https://thelasallian.com/wp-json/wp/v2/posts?tags=2090&?_fields=date,guid,title,excerpt,authors");
     } else if(session_status() == PHP_SESSION_ACTIVE){
         $dateInitiallyAccessed = new DateTime($_SESSION["TIME_ACCESSED"]);
         $dateNow = new DateTime(time());
         $difference = date_diff($dateInitiallyAccessed, $dateNow);
         // TODO: WRITE TIME CHECKER FUNCTION
     }
+?>
+
+<?php 
+
+/**
+ * 
+ * 
+ * @param string URL of the request 
+ * @param string Request required. Defaults to GET.
+ * @return assoc_array Returns a JSON santiized associative array of the API.
+ */
+function fetchInfo($url, $httpReq = 'GET'){
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => $httpReq,
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return json_decode($response, true);
+}
+
 ?>
