7cce0cbf3b5cff4ec8e6b41465c1c7bda1671448
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-03-25 21:36:34 +0800

    Added Bootstrap Icons CDN

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 	<!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/css.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
