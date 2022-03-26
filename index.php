f9eec3f129ac091c35f5b8f4b0e4e2f4f2f53f16
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-03-26 17:32:33 +0800

    Added Bootstrap pills for Debate Highlights (Presidential, Vice Presidential, etc.)

--- index.php
+++ index.php
@@ -38,7 +38,48 @@
 		<!-- Second Row -->
 		<div class="row">
             <!-- Debate, Forum, Interview Highlights -->
-			<section class="col">Highlights</section>
+			<section class="col">
+				Highlights
+				
+				<!-- Presidentiables, etc. Buttons -->
+				<ul class="nav nav-pills mb-3 justify-content-end" id="pills-tab" role="tablist">
+					<li class="nav-item" role="presentation">
+						<button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Presidentiables</button>
+					</li>
+					<li class="nav-item" role="presentation">
+						<button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Vice Presidentiables</button>
+					</li>
+					<li class="nav-item" role="presentation">
+						<button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Senatoriables</button>
+					</li>
+				</ul>
+
+				<!-- Main Highlights Container -->
+				<div class="tab-content" id="pills-tabContent">
+					<!-- Presidentiables -->
+					<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+						Pres
+
+					</div>
+
+					<!-- Vice Presidentiables -->
+					<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+						VP
+
+					</div>
+					
+					<!-- Senatoriables -->
+					<div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+						Sen
+
+					</div>
+				</div>
+
+
+
+
+
+			</section>
 		</div>
 		<!-- Third Row -->
 		<div class="row">
