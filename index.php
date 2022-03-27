8bed5af53a55bb4ef8da618e8ecb30120ab8f65d
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-03-27 02:57:07 -0700

    Added require_once() call pointing to rest.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
+	<?php require_once("php/rest.php")?> <!-- Execute php rest script -->
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
