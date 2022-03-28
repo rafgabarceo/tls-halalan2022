59ccb99d78b02a10dc15bfd0ef4341673261bff9
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-03-28 22:47:35 +0800

    Center-aligned Highlights' Bootstrap pills

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 				Highlights
 				
 				<!-- Presidentiables, etc. Buttons -->
-				<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+				<ul class="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
 					<li class="nav-item" role="presentation">
 						<button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Presidentiables</button>
 					</li>
