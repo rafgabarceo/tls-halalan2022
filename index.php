862b860ec05be74b08628abd5b83202d3ed468c0
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-04-10 12:08:31 +0800

    Added social media meta tags

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<!-- Meta Tags for FB and Twitter -->
+	<meta property="og:url" content="http://halalan2022.thelasallian.com/" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="The LaSallian 2022 National Elections" />
+	<meta property="og:image" content="FB-TWT-Thumbnail.png" />
+	<meta name="twitter:card" content="summary"></meta>
+	<meta name="twitter:site" content="@TheLaSallian"></meta>
+
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
